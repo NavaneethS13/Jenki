85a2554fc4e1b29e1a73f1913883856022d18fe1
NavaneethS13 <vvce22cseaiml0111@vvce.ac.in>
2025-03-03 15:45:50 +0530

    Update and rename areaofcircle.java to Areaofcircle.java

--- Areaofcircle.java
+++ Areaofcircle.java
@@ -1,12 +1,12 @@
 // Java program to calculate the area of the
-public class GFG {
+public class Areaofcircle {
 	public static void main(String[] args)
 	{
 		int radius;
-		double pi = 3.142, area;
-		int radius = 5;
+		pi = 3.142, area;
+		radius = 5;
 		// calculating the area of the circle
-		float area = pi * radius * radius;
+		area = pi * radius * radius;
 		// printing the area of the circle
 		System.out.println("Area of circle is :" + area);
 	}
